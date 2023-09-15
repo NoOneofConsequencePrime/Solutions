f2579d440cb738aa2de17c9e9e6ff844f985deeb
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-09-15 10:16:12 -0400

    O(n) time, O(n) memory

---
+++ LeetCode/majority-element.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+    unordered_map<int, int> um;
+    int n = nums.size();
+    for (auto& x : nums) {
+        um[x]++;
+        if (um[x] > n/2) return x;
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> a = {2,2,1,1,1,2,2};
+    cout << majorityElement(a) << '\n';
+
+    return 0;
+}
