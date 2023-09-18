d3fde71b8e7c8da68f549957684e6161d668cd44
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-09-18 10:02:29 -0400

    O(n) time, O(n) space (smaller)

--- LeetCode/rotate-array.cpp
+++ LeetCode/rotate-array.cpp
@@ -8,8 +8,9 @@ using namespace std;
 void rotate(vector<int>& nums, int k) {
     int n = nums.size();
     k %= n;
-    for (int i = 0; i < n-k; i++) nums.push_back(nums[i]);
-    nums.erase(nums.begin(), nums.begin()+(n-k));
+    reverse(nums.begin()+n-k, nums.end());
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin()+k, nums.end());
 }
 
 int main() {
