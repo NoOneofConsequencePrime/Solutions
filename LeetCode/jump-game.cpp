f1dcc1b548a9db7c73bd8e9cb2af1c31b37976b7
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-09-18 11:07:52 -0400

    O(n) time, O(1) space

--- LeetCode/jump-game.cpp
+++ LeetCode/jump-game.cpp
@@ -8,24 +8,16 @@
 using namespace std;
 
 bool canJump(vector<int>& nums) {
-    int i = nums.size()-1;
-    while (i != 0) {
-        bool flag = true;
-        for (int j = 0; j < i; j++) {
-            if (i-j <= nums[j]) {
-                i = j;
-                flag = false;
-            }
-        }
-        
-        if (flag) return false;
+    int j = 0;
+    for (int i = 0; i <= j && i < nums.size(); i++) {
+        j = max(j, i+nums[i]);
     }
 
-    return true;
+    return j >= nums.size()-1;
 }
 
 int main() {
-    vector<int> v = {3,2,1,0,4};
+    vector<int> v = {2,3,1,1,4};
     cout << canJump(v);
 
     return 0;
