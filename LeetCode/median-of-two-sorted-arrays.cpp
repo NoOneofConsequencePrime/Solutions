3fd86cef1c643ecf13d4aacff3d61d9e5caf58b1
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-09-21 00:28:22 -0400

    O(n+m) time, O(n+m) space

--- LeetCode/median-of-two-sorted-arrays.cpp
+++ LeetCode/median-of-two-sorted-arrays.cpp
@@ -11,9 +11,30 @@ using namespace std;
 
 double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
     vector<int> v;
-    for (int& x : nums1) v.push_back(x);
-    for (int& x : nums2) v.push_back(x);
-    sort(v.begin(), v.end());
+    int i = 0, j = 0;
+    while (true) {
+        if (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                v.push_back(nums1[i]);
+                i++;
+            } else {
+                v.push_back(nums2[j]);
+                j++;
+            }
+        } else if (i < nums1.size()) {
+            while (i < nums1.size()) {
+                v.push_back(nums1[i]);
+                i++;
+            }
+            break;
+        } else if (j < nums2.size()) {
+            while (j < nums2.size()) {
+                v.push_back(nums2[j]);
+                j++;
+            }
+            break;
+        } else break;
+    }
     int n = v.size();
 
     return (double)(v[n/2]+v[(n-1)/2])/2;
