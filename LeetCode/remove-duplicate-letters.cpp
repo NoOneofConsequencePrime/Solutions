e74ba55519dc8a850cc19d13a78520f558ba646e
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-09-26 20:08:10 -0400

    O(n) time, O(1) space (alphabet)

---
+++ LeetCode/remove-duplicate-letters.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <unordered_set>
+#include <numeric>
+using namespace std;
+
+string removeDuplicateLetters(string s) {
+    int lApp[30];
+    for (int i = 0; i < s.length(); i++) lApp[s[i]-'a'] = i;
+
+    stack<char> st;
+    bool chk[30]; memset(chk, false, sizeof(chk));
+    for (int i = 0; i < s.length(); i++) {
+        if (chk[s[i]-'a']) continue;
+        while (!st.empty() && st.top() > s[i] && i < lApp[st.top()-'a']) {
+            chk[st.top()-'a'] = false; st.pop();
+        }
+        st.push(s[i]); chk[s[i]-'a'] = true;
+    }
+
+    string ret = "";
+    while (!st.empty()) {
+        ret += st.top();
+        st.pop();
+    }
+    reverse(ret.begin(), ret.end());
+
+    return ret;
+}
+
+int main() {
+    cout << removeDuplicateLetters("cbacdcbc");
+
+    return 0;
+}
