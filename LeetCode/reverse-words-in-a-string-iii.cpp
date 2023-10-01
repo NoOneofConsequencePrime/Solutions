e8e35230a8e7a24d91f3e8ce9d0bef4b3ba39915
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-10-01 18:35:49 -0400

    O(n) time, O(1) space

--- LeetCode/reverse-words-in-a-string-iii.cpp
+++ LeetCode/reverse-words-in-a-string-iii.cpp
@@ -10,23 +10,22 @@
 using namespace std;
 
 string reverseWords(string s) {
-    string ret = "", tmp = "";
-    for (char& c : s) {
-        if (c != ' ') tmp += c;
-        else {
-            reverse(tmp.begin(), tmp.end());
-            ret += tmp+" ";
-            tmp = "";
+    for (int i = 0, j = 1; j < s.length(); j++) {
+        printf("(%d, %d)\n", i, j);
+        if (s[j] == ' ') {
+            for (int a = 0; a < (i+j)/2-i; a++) swap(s[i+a], s[j-a-1]);
+            i = j+1;
+        } else if (j == s.length()-1) {
+            j++;
+            for (int a = 0; a < (i+j)/2-i; a++) swap(s[i+a], s[j-a-1]);
         }
     }
-    reverse(tmp.begin(), tmp.end());
-    ret += tmp+" ";
 
-    return ret.substr(0, ret.length()-1);
+    return s;
 }
 
 int main() {
-    cout << reverseWords("Let's take LeetCode contest");
+    cout << reverseWords("abc cba");
 
     return 0;
 }
