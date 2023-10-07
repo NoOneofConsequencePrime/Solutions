185a80edd3bfb6f9b4d5b18599f87eaccf037a93
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-10-06 23:55:36 -0400

    init

---
+++ LeetCode/design-hashmap.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <unordered_set>
+#include <numeric>
+using namespace std;
+
+class MyHashMap {
+private:
+    int arr[1e5];
+public:
+    MyHashMap() {}
+    
+    void put(int key, int value) {
+        
+    }
+    
+    int get(int key) {
+        
+    }
+    
+    void remove(int key) {
+        
+    }
+};
+
+int main() {
+    vector<int> v = {0,1,0,2,1,0,1,3,2,1,2,1};
+    cout << trap(v);
+
+    return 0;
+}
