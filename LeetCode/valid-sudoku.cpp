3b7e5e3a8509296d148ea24d15e23a7c3bdb936f
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-10-19 19:13:21 -0400

    O(nm) time, O(1) space (single int)

--- LeetCode/valid-sudoku.cpp
+++ LeetCode/valid-sudoku.cpp
@@ -10,31 +10,31 @@
 using namespace std;
 
 bool isValidSudoku(vector<vector<char>>& board) {
-    bool cnt[9];
+    int chk = 0;
     for (int i = 0; i < 9; i++) {
-        memset(cnt, 0, sizeof(cnt));
+        chk = 0;
         for (int j = 0; j < 9; j++) {
             if (board[i][j] == '.') continue;
-            if (cnt[board[i][j]-'1']) return false;
-            cnt[board[i][j]-'1'] = 1;
+            if ((chk >> board[i][j]-'1')&1) return false;
+            chk |= 1 << board[i][j]-'1';
         }
     }
     for (int j = 0; j < 9; j++) {
-        memset(cnt, 0, sizeof(cnt));
+        chk = 0;
         for (int i = 0; i < 9; i++) {
             if (board[i][j] == '.') continue;
-            if (cnt[board[i][j]-'1']) return false;
-            cnt[board[i][j]-'1'] = 1;
+            if ((chk >> board[i][j]-'1')&1) return false;
+            chk |= 1 << board[i][j]-'1';
         }
     }
     for (int a = 0; a < 3; a++) {
         for (int b = 0; b < 3; b++) {
-            memset(cnt, 0, sizeof(cnt));
+            chk = 0;
             for (int i = a*3; i < a*3+3; i++) {
                 for (int j = b*3; j < b*3+3; j++) {
                     if (board[i][j] == '.') continue;
-                    if (cnt[board[i][j]-'1']) return false;
-                    cnt[board[i][j]-'1'] = 1;
+                    if ((chk >> board[i][j]-'1')&1) return false;
+                    chk |= 1 << board[i][j]-'1';
                 }
             }
         }
