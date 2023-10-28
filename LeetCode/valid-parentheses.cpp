3aabe1eb28650f21f76560208c095acfd2cca789
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-10-27 23:05:03 -0400

    O(n) time, O(n) space

---
+++ LeetCode/valid-parentheses.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <unordered_set>
+#include <numeric>
+using namespace std;
+
+bool isValid(string s) {
+    stack<char> stk;
+    for (char& c : s) {
+        if (c == '(' || c == '[' || c == '{') stk.push(c);
+        else {
+            if (stk.empty()) return false;
+
+            if (c == ')' && stk.top() == '(') stk.pop();
+            else if (c == ']' && stk.top() == '[') stk.pop();
+            else if (c == '}' && stk.top() == '{') stk.pop();
+            else return false;
+        }
+    }
+    
+    return stk.empty();
+}
+
+int main() {
+    cout << isValid("(}");
+
+    return 0;
+}
