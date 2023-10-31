8521b3ceca7cccd0529449b18b1c038a9635f570
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-10-31 13:40:58 -0400

    O(n) time (faster), O(n) space (smaller)

--- LeetCode/count-vowels-permutation.cpp
+++ LeetCode/count-vowels-permutation.cpp
@@ -12,37 +12,23 @@ using namespace std;
 vector<int> dp[5];
 const int M = 1e9+7;
 
-int fun(int n, char c) {
-    if (n == 1) return 1;
-
-    switch (c) {
-        case 'a':
-            if (dp[0][n-1] != 0) return dp[0][n-1];
-            return dp[0][n-1] = fun(n-1, 'e');
-        case 'e':
-            if (dp[1][n-1] != 0) return dp[1][n-1];
-            return dp[1][n-1] = (fun(n-1, 'a')+fun(n-1, 'i'))%M;
-        case 'i':
-            if (dp[2][n-1] != 0) return dp[2][n-1];
-            return dp[2][n-1] = ((fun(n-1, 'a')+fun(n-1,'e'))%M+(fun(n-1, 'o')+fun(n-1, 'u'))%M)%M;
-        case 'o':
-            if (dp[3][n-1] != 0) return dp[3][n-1];
-            return dp[3][n-1] = (fun(n-1, 'i')+fun(n-1, 'u'))%M;
-        case 'u':
-            if (dp[4][n-1] != 0) return dp[4][n-1];
-            return dp[4][n-1] = fun(n-1, 'a');
+int countVowelPermutation(int n) {
+    for (int i = 0; i < 5; i++) {
+        dp[i].resize(n, 0); dp[0][i] = 1;
+    }
+    for (int i = 1; i < n; i++) {
+        dp[i][0] = dp[i-1][1];
+        dp[i][1] = (dp[i-1][0]+dp[i-1][2])%M;
+        dp[i][2] = ((dp[i-1][0]+dp[i-1][1])%M+(dp[i-1][3]+dp[i-1][4])%M)%M;
+        dp[i][3] = (dp[i-1][2]+dp[i-1][4])%M;
+        dp[i][4] = dp[i-1][0];
     }
 
-    return -1;
-}
-
-int countVowelPermutation(int n) {
-    for (int i = 0; i < 5; i++) dp[i].resize(n, 0);
-    return (((fun(n, 'a')+fun(n, 'e'))%M+(fun(n, 'i')+fun(n, 'o'))%M)%M+fun(n, 'u'))%M;
+    return (((dp[n-1][0]+dp[n-1][1])%M+(dp[n-1][2]+dp[n-1][3])%M)%M+dp[n-1][4])%M;
 }
 
 int main() {
-    cout << countVowelPermutation(2);
+    cout << countVowelPermutation(1);
 
     return 0;
 }
