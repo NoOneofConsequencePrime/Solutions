a1a1a2a757604e6abf950bc28d0576b3db8f3503
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-10-31 13:46:59 -0400

    O(n) time (faster), O(1) space

--- LeetCode/count-vowels-permutation.cpp
+++ LeetCode/count-vowels-permutation.cpp
@@ -9,26 +9,25 @@
 #include <numeric>
 using namespace std;
 
-vector<int> dp[5];
 const int M = 1e9+7;
 
 int countVowelPermutation(int n) {
-    for (int i = 0; i < 5; i++) {
-        dp[i].resize(n, 0); dp[0][i] = 1;
-    }
-    for (int i = 1; i < n; i++) {
-        dp[i][0] = dp[i-1][1];
-        dp[i][1] = (dp[i-1][0]+dp[i-1][2])%M;
-        dp[i][2] = ((dp[i-1][0]+dp[i-1][1])%M+(dp[i-1][3]+dp[i-1][4])%M)%M;
-        dp[i][3] = (dp[i-1][2]+dp[i-1][4])%M;
-        dp[i][4] = dp[i-1][0];
+    int a, e, i, o, u; a = e = i = o = u = 1;
+    for (int j = 2; j <= n; j++) {
+        int A = e;
+        int E = (a+i)%M;
+        int I = ((a+e)%M+(o+u)%M)%M;
+        int O = (i+u)%M;
+        int U = a;
+
+        a = A; e = E; i = I; o = O; u = U;
     }
 
-    return (((dp[n-1][0]+dp[n-1][1])%M+(dp[n-1][2]+dp[n-1][3])%M)%M+dp[n-1][4])%M;
+    return (((a+e)%M+(i+o)%M)%M+u)%M;
 }
 
 int main() {
-    cout << countVowelPermutation(1);
+    cout << countVowelPermutation(2);
 
     return 0;
 }
