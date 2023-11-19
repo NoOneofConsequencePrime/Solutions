dc4b2eac353cce746ee0fcda283aa7ebad459b6d
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-11-19 18:54:34 -0500

    O(nlogn) time, O(n) space

--- LeetCode/maximum-element-after-decreasing-and-rearranging.cpp
+++ LeetCode/maximum-element-after-decreasing-and-rearranging.cpp
@@ -7,16 +7,19 @@
 #include <deque>
 #include <unordered_set>
 #include <numeric>
+#include <map>
 using namespace std;
 
 int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-    sort(arr.begin(), arr.end());
-    arr[0] = 1;
-    for (int i = 1; i < arr.size(); i++) {
-        if (arr[i]-arr[i-1] > 1) arr[i] = arr[i-1]+1;
+    map<int, int> mp;
+    for (int& x : arr) mp[x]++;
+
+    int ret = (mp.count(1)==1);
+    for (auto it = mp.begin(); it != mp.end(); it++) {
+        ret += min((*it).first-ret, (*it).second);
     }
     
-    return arr[arr.size()-1];
+    return ret;
 }
 
 int main() {
