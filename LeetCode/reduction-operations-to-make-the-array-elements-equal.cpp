4c9f8c453b4dc11e0d833dd5f0af75a3653673c4
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-11-19 18:19:42 -0500

    O(nlogn) time, O(n) space (larger)

--- LeetCode/reduction-operations-to-make-the-array-elements-equal.cpp
+++ LeetCode/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -9,25 +9,27 @@
 #include <numeric>
 using namespace std;
 
-const int MM = 5e4+2;
-int freq[MM];
-
 int reductionOperations(vector<int>& nums) {
-    for (int& x : nums) freq[x]++;
-    
-    int ret = 0;
-    for (int i = MM-1, tmp = 0; i >= 1; i--) {
-        if (freq[i] != 0) {
-            tmp += freq[i];
-            ret += tmp;
-        }
+    sort(nums.begin(), nums.end());
+    vector<int> freq; unordered_map<int, int> vi;
+    for (int& x : nums) {
+        if (vi.count(x) == 0) {
+            vi[x] = freq.size();
+            freq.push_back(1);
+        } else freq[vi[x]]++;
     }
     
-    return ret - nums.size();
+    int ret = 0;
+    for (int i = freq.size()-1, tmp = 0; i >= 0; i--) {
+        tmp += freq[i];
+        ret += tmp;
+    }
+    
+    return ret-nums.size();
 }
 
 int main() {
-    vector<int> v = {1,1,2,2,3};
+    vector<int> v = {10,10,10,10,1,10,10,10,10,10};
     cout << reductionOperations(v);
 
     return 0;
