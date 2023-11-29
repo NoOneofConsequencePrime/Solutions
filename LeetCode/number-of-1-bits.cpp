e12e4ec8d96235608a2bbf390d4cfdf88242cc27
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-11-28 19:18:11 -0500

    O(n) time (faster)

--- LeetCode/number-of-1-bits.cpp
+++ LeetCode/number-of-1-bits.cpp
@@ -11,7 +11,13 @@
 using namespace std;
 
 int hammingWeight(uint32_t n) {
-    return __builtin_popcount(n);
+    int ret = 0;
+    while (n) {
+        n &= (n-1);
+        ret++;
+    }
+
+    return ret;
 }
 
 int main() {
