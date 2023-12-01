716de3923287216ce75d5e536d18fa54ffccd51c
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-11-30 23:54:56 -0500

    test.cpp

---
+++ LeetCode/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <unordered_set>
+#include <numeric>
+using namespace std;
+
+
+int main() {
+    vector<int> a = {1,2,3,2}, b = {1,2,3,2};
+    sort(a.begin(), a.end(), cmp);
+
+    return 0;
+}
