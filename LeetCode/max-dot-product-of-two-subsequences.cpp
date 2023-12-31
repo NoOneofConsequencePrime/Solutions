e6166fb6438a96f17f47d13b497c6fc3cddb4653
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2023-12-31 14:21:28 -0500

    O(n^2) time (faster), O(n^2) space

--- LeetCode/max-dot-product-of-two-subsequences.cpp
+++ LeetCode/max-dot-product-of-two-subsequences.cpp
@@ -12,46 +12,35 @@ using namespace std;
 const int MM = 502;
 int dp[MM][MM];
 
-int fun(int x, int y, vector<int>& a, vector<int>& b) {
-    if (dp[x][y] < 1e9) return dp[x][y];
-    if (x >= a.size() || y >= b.size()) return 0;
-
-    int ret = max(fun(x+1, y, a, b), fun(x, y+1, a, b));
-    for (int i = x; i < a.size(); i++) {
-        ret = max(ret, a[i]*b[y] + fun(i+1, y+1, a, b));
-    }
-    for (int i = y; i < b.size(); i++) {
-        ret = max(ret, a[x]*b[i] + fun(x+1, i+1, a, b));
-    }
-
-    return dp[x][y] = ret;
-}
-
 int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-    memset(dp, 0x3f, sizeof(dp));
-    int ret = fun(0, 0, nums1, nums2), mx = INT_MIN;
+    int mx = nums1[0]*nums2[0];
 
-    for (int i = 0; i < nums1.size(); i++) {
-        for (int j = 0; j < nums2.size(); j++) {
-            if ((mx = max(mx, nums1[i]*nums2[j])) >= 0) return ret;
+    dp[0][0] = max(0, nums1[0]*nums2[0]);
+    for (int i = 1; i < nums1.size(); i++) {
+        mx = max(mx, nums1[i]*nums2[0]);
+        dp[i][0] = max(dp[i-1][0], nums1[i]*nums2[0]);
+    }
+    for (int i = 1; i < nums2.size(); i++) {
+        mx = max(mx, nums1[0]*nums2[i]);
+        dp[0][i] = max(dp[0][i-1], nums1[0]*nums2[i]);
+    }
+
+    for (int i = 1; i < nums1.size(); i++) {
+        for (int j = 1; j < nums2.size(); j++) {
+            mx = max(mx, nums1[i]*nums2[j]);
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j-1] + nums1[i]*nums2[j]);
+            dp[i][j] = max(dp[i][j], max(dp[i-1][j], dp[i][j-1]));
         }
     }
 
-    return mx;
+    int ret = dp[nums1.size()-1][nums2.size()-1];
+
+    return (mx >= 0)? ret : mx;
 }
 
 int main() {
-    // vector<int> a = {-5,3,-5,-3,1}, b = {-2,4,2,5,-5};
-    // vector<int> a = {2,1,-2,5}, b = {3,0,-6};
-    vector<int> a = {3,-2}, b = {2,-6,7};
-    maxDotProduct(a, b);
-    for (int i = 0; i < a.size(); i++) {
-        for (int j = 0; j < b.size(); j++) {
-            printf("(%d, %d - %d) ", a[i], b[j], dp[i][j]);
-        }
-        cout << endl;
-    }
-    // cout << maxDotProduct(a, b);
+    vector<int> a = {1, -1}, b = {-1, 1};
+    cout << maxDotProduct(a, b);
 
     return 0;
 }
