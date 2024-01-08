a2edf759bdeedb2ccd98dfc2123fcc71fc76d5e5
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-08 10:23:13 -0500

    O(n^2) time (faster), O(n^2) space (smaller)

--- LeetCode/arithmetic-slices-ii-subsequence.cpp
+++ LeetCode/arithmetic-slices-ii-subsequence.cpp
@@ -13,20 +13,24 @@ using namespace std;
 //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
 const int MM = 1002;
-unordered_map<int, int> dp[MM];
+unordered_map<int, vector<int>> mp;
+int dp[MM][MM];
 
 int numberOfArithmeticSlices(vector<int>& nums) {
     int n = nums.size(), ret = 0;
+    for (int i = 0; i < n; i++) mp[nums[i]].push_back(i);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i; j++) {
-            long long chg = (long long)nums[i]-nums[j];
-            if (chg > INT_MAX || chg < INT_MIN) continue;
-
-            dp[i][chg]++;
-            if (dp[j].count(chg) != 0) {
-                dp[i][chg] += dp[j][chg];
-                ret += dp[j][chg];
+            long long prev = 2ll*nums[j]-nums[i];
+            if (prev < INT_MIN || prev > INT_MAX) continue;
+            if (mp.count(prev) != 0) {
+                vector<int>& v = mp[prev];
+                for (int x : v) {
+                    if (x >= j) break;
+                    dp[i][j] += dp[j][x]+1;
+                }
             }
+            ret += dp[i][j];
         }
     }
 
