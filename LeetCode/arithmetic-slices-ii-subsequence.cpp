a37877c260b4b273fb88c9e75a756b696b24f05e
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-08 08:38:00 -0500

    O(n^3) time, O(n^2) space

--- LeetCode/arithmetic-slices-ii-subsequence.cpp
+++ LeetCode/arithmetic-slices-ii-subsequence.cpp
@@ -12,40 +12,22 @@ using namespace std;
 
 //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-unordered_map<long long, vector<long long>> mp;
-unordered_map<int, int> cnt;
-
-long long hsh(long long a, long long b, int c) {// val, chg
-    return (long long)a*1e10+b+c*1e5;
-}
+const int MM = 1002;
+unordered_map<long long, vector<int>> dp[MM];
 
 int numberOfArithmeticSlices(vector<int>& nums) {
     int n = nums.size(), ret = 0;
     for (int i = 0; i < n; i++) {
-        for (int j = i-1; j >= 0; j--) {
-            long long chg = nums[i]-nums[j];
-            if (chg == 0) continue;
-            // printf("%lld, %d\n", hsh(nums[j], chg, j), mp[hsh(nums[j], chg, j)]);
-            if (mp.count(hsh(nums[j], chg, j)) != 0) {
-                for (int x : mp[hsh(nums[j], chg, j)]) mp[hsh(nums[i], chg, i)].push_back(x+1);
-            }
-            mp[hsh(nums[i], chg, i)].push_back(0);
-        }
-        cnt[nums[i]]++;
-    }
-
-    for (auto& [key, val] : cnt) {
-        if (val < 3) continue;
-        int sum = 1, prev = 1; val -= 2;
-        for (int i = 2; i <= val; i++) {
-            prev = sum+1+prev+1;
-            sum += prev;
-        }
-        ret += sum;
-    }
-    for (auto& [key, v] : mp) {
-        for (long long x : v) {
-            ret += x*(x+1)/2;
+        for (int j = 0; j < i; j++) {
+            long long chg = (long long)nums[i]-nums[j];
+            if (dp[j].count(chg) != 0) {
+                int tmp = 0;
+                for (int x : dp[j][chg]) {
+                    tmp += x+1;
+                    ret += x+1;
+                }
+                dp[i][chg].push_back(tmp);
+            } else dp[i][chg].push_back(0);
         }
     }
 
@@ -53,21 +35,7 @@ int numberOfArithmeticSlices(vector<int>& nums) {
 }
 
 int main() {
-    vector<int> v = {2,4,6,8,10};
-    //  234
-    // 1234
-    // 1 34
-
-    // 1+2+2+6+5
-    // +(sum+1)+(prev+1) = prev
-
-    //   345
-    //  2 45
-    //  2345
-    // 1  45
-    // 1 345
-    // 12 45
-    // 12345
+    vector<int> v (1000,1000);
     cout << numberOfArithmeticSlices(v);
 
     return 0;
