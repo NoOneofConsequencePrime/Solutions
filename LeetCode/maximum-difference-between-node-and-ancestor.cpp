55518dee04fe5b5ee6911c89b906d00cd7c05f06
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-10 23:06:16 -0500

    O(n) time, O(n) space

--- LeetCode/maximum-difference-between-node-and-ancestor.cpp
+++ LeetCode/maximum-difference-between-node-and-ancestor.cpp
@@ -12,31 +12,16 @@ using namespace std;
 
 //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-#define f first
-#define s second
-typedef pair<TreeNode*, pair<int, int>> p;
+int dfs(TreeNode* root, int mx, int mi) {
+    if (!root) {return mx-mi;}
+
+    int tmpMx = max(root->val, mx), tmpMi = min(root->val, mi);
+
+    return max(dfs(root->left, tmpMx, tmpMi), dfs(root->right, tmpMx, tmpMi));
+}
 
 int maxAncestorDiff(TreeNode* root) {
-    int ret = 0;
-    queue<p> q; q.push({root, {root->val, root->val}});
-    while (!q.empty()) {
-        p u = q.front(); q.pop();
-        ret = max(ret, max(abs(u.f->val - u.s.f), abs(u.f->val - u.s.s)));
-        if (u.f->left) {
-            p tmp = {u.f->left, {INT_MAX, INT_MIN}};
-            tmp.s.f = min(u.s.f, u.f->val);
-            tmp.s.s = max(u.s.s, u.f->val);
-            q.push(tmp);
-        }
-        if (u.f->right) {
-            p tmp = {u.f->right, {INT_MAX, INT_MIN}};
-            tmp.s.f = min(u.s.f, u.f->val);
-            tmp.s.s = max(u.s.s, u.f->val);
-            q.push(tmp);
-        }
-    }
-
-    return ret.f;
+    return dfs(root, root->val, root->val);
 }
 
 int main() {
