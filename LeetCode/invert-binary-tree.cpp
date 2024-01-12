23e5b84fd721aeee50b37bc42bebab722eecb482
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-12 16:50:33 -0500

    O(n) time, O(n) space

--- LeetCode/invert-binary-tree.cpp
+++ LeetCode/invert-binary-tree.cpp
@@ -12,16 +12,10 @@ using namespace std;
 
 //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-void invertNode(TreeNode* node) {
-    if (!node) {return;}
-    swap(node->left, node->right);
-    invertNode(node->left); invertNode(node->right);
-
-    return;
-}
-
 TreeNode* invertTree(TreeNode* root) {
-    invertNode(root);
+    if (!root) return nullptr;
+    swap(root->left, root->right);
+    invertTree(root->left); invertTree(root->right);
 
     return root;
 }
