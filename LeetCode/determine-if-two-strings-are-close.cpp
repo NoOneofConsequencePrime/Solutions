f7d128898b01e1f9cd5632822aaba9431abc9fa4
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-13 22:21:17 -0500

    O(n) time, O(n) space

---
+++ LeetCode/determine-if-two-strings-are-close.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <unordered_set>
+#include <numeric>
+#include <map>
+using namespace std;
+
+//ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+int cnt1[30], cnt2[30];
+unordered_map<int, int> mp1, mp2;
+
+bool closeStrings(string word1, string word2) {
+    if (word1.length() != word2.length()) return false;
+    
+    int n = word1.length();
+    for (int i = 0; i < n; i++) {
+        cnt1[word1[i]-'a']++; cnt2[word2[i]-'a']++;
+    }
+    for (int i = 0; i < 26; i++) {
+        if (cnt1[i]>0 && cnt2[i]==0) {return false;}
+        if (cnt1[i]==0 && cnt2[i]>0) {return false;}
+
+        if (cnt1[i] > 0) {mp1[cnt1[i]]++;}
+        if (cnt2[i] > 0) {mp2[cnt2[i]]++;}
+    }
+
+    for (auto [f, s] : mp1) {
+        if (mp1.count(f)==0) {return false;}
+        if (mp2[f] != s) {return false;}
+        mp2[f] -= s;
+    }
+    for (auto [f, s] : mp2) {
+        if (s != 0) {return false;}
+    }
+
+    return true;
+}
+
+int main() {
+    cout << closeStrings("cabbba", "abbccc");
+
+    return 0;
+}
