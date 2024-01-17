46d33edf72a6b9957ff6ea163a16b211459720dd
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-17 10:47:17 -0500

    O(nm) time, O(nm) space (smaller)

--- LeetCode/number-of-islands.cpp
+++ LeetCode/number-of-islands.cpp
@@ -21,22 +21,16 @@ typedef pair<ll, ll> pll;
 
 //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-const int MM = 302;
-bitset<MM> vst[MM];
 int mx[4] = {1, -1, 0, 0}, my[4] = {0, 0, 1, -1};
 
 void ffill(vector<vector<char>>& grid, int x, int y) {
     int n = grid.size(), m = grid[0].size();
-    vst[x][y] = 1;
-    queue<pii> q; q.push({x, y});
-    while (!q.empty()) {
-        pii u = q.front(); q.pop();
-        for (int i = 0; i < 4; i++) {
-            pii v = {u.f+mx[i], u.s+my[i]};
-            if (v.f>=0 && v.f<n && v.s>=0 && v.s<m && grid[v.f][v.s]=='1' && !vst[v.f][v.s]) {
-                vst[v.f][v.s] = 1;
-                q.push(v);
-            }
+    grid[x][y] = '0';
+
+    for (int i = 0; i < 4; i++) {
+        pii v = {x+mx[i], y+my[i]};
+        if (v.f>=0 && v.f<n && v.s>=0 && v.s<m && grid[v.f][v.s]=='1') {
+            ffill(grid, v.f, v.s);
         }
     }
 }
@@ -45,7 +39,7 @@ int numIslands(vector<vector<char>>& grid) {
     int n = grid.size(), m = grid[0].size(), ret = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (grid[i][j]=='1' && !vst[i][j]) {
+            if (grid[i][j]=='1') {
                 ret++;
                 ffill(grid, i, j);
             }
