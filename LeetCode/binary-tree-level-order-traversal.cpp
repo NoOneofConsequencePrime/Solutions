51bbf8ee47e9380104a610afd5111a9bf9c15971
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-19 09:56:47 -0500

    O(n) time, O(n) space (smaller?)

--- LeetCode/binary-tree-level-order-traversal.cpp
+++ LeetCode/binary-tree-level-order-traversal.cpp
@@ -25,22 +25,19 @@ vector<vector<int>> levelOrder(TreeNode* root) {
     vector<vector<int>> ret;
     if (!root) {return ret;}
 
-    queue<pair<TreeNode*, int>> q; q.push({root->left, 1}); q.push({root->right, 1});
-    vector<int> tmpV = {root->val};
-    int curDep = 0;
+    queue<TreeNode*> q; q.push(root);
     while (!q.empty()) {
-        auto [cur, dep] = q.front(); q.pop();
-        if (!cur) {continue;}
-        if (dep != curDep) {
-            curDep++;
-            ret.push_back(tmpV);
-            tmpV.clear();
+        vector<int> tmpV;
+        int n = q.size();
+        while (n > 0) {
+            TreeNode* cur = q.front(); q.pop();
+            tmpV.push_back(cur->val);
+            if (cur->left) {q.push(cur->left);}
+            if (cur->right) {q.push(cur->right);}
+            n--;
         }
-        tmpV.push_back(cur->val);
-
-        q.push({cur->left, dep+1}); q.push({cur->right, dep+1});
+        ret.push_back(tmpV);
     }
-    ret.push_back(tmpV);
 
     return ret;
 }
