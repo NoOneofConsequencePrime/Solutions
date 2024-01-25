c4deb4910c9350bfb3550e2ffc4982209dee749c
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-24 20:17:31 -0500

    O(nm) time, O(m) space

--- LeetCode/longest-common-subsequence.cpp
+++ LeetCode/longest-common-subsequence.cpp
@@ -22,21 +22,24 @@ typedef pair<ll, ll> pll;
 //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
 int longestCommonSubsequence(string text1, string text2) {
+    if (text1.length() < text2.length()) {swap(text1, text2);}// text1 longer
     int n1 = text1.length(), n2 = text2.length();
-    vector<vector<int>> dp(n1, vector<int>(n2, 0));
+    vector<vector<int>> dp(2, vector<int>(n2, 0));
 
     for (int i = 0; i < n1; i++) {
         for (int j = 0; j < n2; j++) {
             if (text1[i] == text2[j]) {
-                if (i > 0 && j > 0) {dp[i][j] = dp[i-1][j-1];}
-                dp[i][j]++;
+                if (i > 0 && j > 0) {dp[1][j] = dp[0][j-1];}
+                dp[1][j]++;
             }
-            if (i > 0) {dp[i][j] = max(dp[i][j], dp[i-1][j]);}
-            if (j > 0) {dp[i][j] = max(dp[i][j], dp[i][j-1]);}
+            if (i > 0) {dp[1][j] = max(dp[1][j], dp[0][j]);}
+            if (j > 0) {dp[1][j] = max(dp[1][j], dp[1][j-1]);}
         }
+        swap(dp[0], dp[1]);
+        fill(dp[1].begin(), dp[1].end(), 0);
     }
 
-    return dp[n1-1][n2-1];
+    return dp[0][n2-1];
 }
 
 int main() {
