4debd4b10f35dd5abd87391495ebe9aeb61823ce
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-26 09:58:42 -0500

    O(nml) time, O(nm) space (smaller)

--- LeetCode/out-of-boundary-paths.cpp
+++ LeetCode/out-of-boundary-paths.cpp
@@ -21,19 +21,19 @@ typedef pair<ll, ll> pll;
 
 //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-static const int M = 1e9+7;
+const int M = 1e9+7;
+const int MM = 52;
 int mx[4] = {1,-1,0,0}, my[4] = {0,0,1,-1};
+int dp[2][MM][MM];
 
 int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-    vector<vector<vector<ll>>> dp(2, vector<vector<ll>>(n, vector<ll>(m, 0)));
     for (int idx = 1; idx <= maxMove; idx++) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                dp[1][i][j] = 0;
                 for (int k = 0; k < 4; k++) {
                     int dx = i+mx[k], dy = j+my[k];
                     if (dx>=0 && dx<n) {
-                        if (dy>=0 && dy<m) {dp[1][i][j] += dp[0][dx][dy];}
+                        if (dy>=0 && dy<m) {dp[1][i][j] = (dp[1][i][j]+dp[0][dx][dy])%M;}
                         else {dp[1][i][j]++;}
                     } else {dp[1][i][j]++;}
                 }
@@ -41,6 +41,7 @@ int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
             }
         }
         swap(dp[0], dp[1]);
+        memset(dp[1], 0, sizeof(dp[1]));
     }
 
     return dp[0][startColumn][startRow];
