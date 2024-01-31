b041f7c51721b2139425c78f41b5d07fdd05ded3
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-01-31 17:36:53 -0500

    O(n) time, O(n) space

---
+++ LeetCode/count-complete-tree-nodes.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <unordered_set>
+#include <numeric>
+#include <map>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> ppi;
+typedef pair<ll, ll> pll;
+#define f first
+#define s second
+
+//ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    void dfs(int &ret, TreeNode *node) {
+        if (!node) {return;}
+        ret++;
+        dfs(ret, node->left); dfs(ret, node->right);
+    }
+public:
+    int countNodes(TreeNode* root) {
+        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+        int ret = 0;
+        dfs(ret, root);
+
+        return ret;
+    }
+};
+
+int main() {
+
+    return 0;
+}
