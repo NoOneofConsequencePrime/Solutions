dd6ed24d7836cfbfb1b020061a0226e14caaa77d
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2024-03-08 22:43:42 -0500

    O(n) time, O(1) space

---
+++ LeetCode/minimum-common-value.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int ret = -1;
+        int n = nums1.size(), m = nums2.size(), i = 0, j = 0;
+        while (i < n && j < m) {
+            if (nums1[i] < nums2[j]) {i++;}
+            else if (nums1[i] > nums2[j]) {j++;}
+            else {
+                ret = nums1[i];
+                break;
+            }
+        }
+
+        return ret;
+    }
+};
