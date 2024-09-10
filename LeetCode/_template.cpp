3611da6cfe086787295725ee0b920c67baea9337
Bryan Zhao <bryanzhao5@outlook.com>
2024-09-10 18:14:34 -0400

    better template

--- LeetCode/_template.cpp
+++ LeetCode/_template.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> ppi;
+typedef pair<ll, ll> pll;
+#define f first
+#define s second
+
+ll modPow(ll b, ll e, ll m) {
+    ll ret = 1;
+    while (e > 0) {
+        if ((e&1ll) == 1) {ret = (ret*b)%m;}
+        e >>= 1;
+        b = (b*b)%m;
+    }
+
+    return ret;
+}
+
+ll gcd(ll a, ll b) {
+    return b==0? a:gcd(b, a%b);
+}
+
 int main() {
 
     return 0;
