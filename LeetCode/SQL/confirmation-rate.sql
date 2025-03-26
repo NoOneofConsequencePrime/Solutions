0d810ea275ae0781d90478500ed7f760a68b8aa3
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-03-26 17:38:11 +0800

    other approach

--- LeetCode/SQL/confirmation-rate.sql
+++ LeetCode/SQL/confirmation-rate.sql
@@ -1,9 +1,12 @@
 select s.user_id
-    , if(c.rate is not null, c.rate, 0) as confirmation_rate
+    , round(
+        avg(
+            case
+                when c.action = 'confirmed' then 1
+                else 0
+            end
+        ), 2
+    ) as confirmation_rate
 from Signups s
-left join (
-    select user_id
-        , round(avg(if(action = 'confirmed', 1, 0)), 2) as rate
-    from Confirmations
-    group by user_id
-) c on c.user_id = s.user_id
+left join Confirmations c on c.user_id = s.user_id
+group by s.user_id
