3012084d522794921dd01299b3887baa64e92718
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-03-26 10:02:11 +0800

    reorganization

--- LeetCode/_template.cpp
+++ LeetCode/_template.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 //ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<int, pii> ppi;
-typedef pair<ll, ll> pll;
-#define f first
-#define s second
+// typedef long long ll;
+// typedef pair<int, int> pii;
+// typedef pair<int, pii> ppi;
+// typedef pair<ll, ll> pll;
+// #define f first
+// #define s second
 
-ll modPow(ll b, ll e, ll m) {
-    ll ret = 1;
-    while (e > 0) {
-        if ((e&1ll) == 1) {ret = (ret*b)%m;}
-        e >>= 1;
-        b = (b*b)%m;
-    }
+// ll modPow(ll b, ll e, ll m) {
+//     ll ret = 1;
+//     while (e > 0) {
+//         if ((e&1ll) == 1) {ret = (ret*b)%m;}
+//         e >>= 1;
+//         b = (b*b)%m;
+//     }
 
-    return ret;
-}
+//     return ret;
+// }
 
-ll gcd(ll a, ll b) {
-    return b==0? a:gcd(b, a%b);
-}
+// ll gcd(ll a, ll b) {
+//     return b==0? a:gcd(b, a%b);
+// }
 
 int main() {
 
--- Other/test.cpp
+++ Other/test.cpp
@@ -1,27 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <queue>
-#include <stack>
-#include <deque>
-#include <unordered_set>
-#include <numeric>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> fun(vector<int> hand) {
-    if (hand.size() == 0) return {};
-    else {
-        hand.pop_front();
-        vector<int> a = fun(hand);
-        for (auto& x : a) printf("%d ", x);
-        printf("\n");
-        a.push_front(hand[0]);
-        for (auto& x : a) printf("%d ", x);
-    }
-}
+//ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-int main(void) {
-    vector<int> v = {1,2,3};
-    cout << v.end()-v.begin();
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<int, pii> ppi;
+typedef pair<ll, ll> pll;
+#define f first
+#define s second
+
+int main() {
+
+    return 0;
 }
