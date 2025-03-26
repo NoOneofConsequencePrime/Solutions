f98df59282308c08182d3d1430e61fe127d04879
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-03-26 15:46:33 +0800

    other approach

--- LeetCode/rising-temperature.sql
+++ LeetCode/rising-temperature.sql
@@ -1,4 +1,4 @@
 select w1.id
-from Weather as w1
-join Weather as w2 on datediff(w1.recordDate, w2.recordDate) = 1
-where w1.temperature > w2.temperature
+from Weather w1, Weather w2
+where w1.recordDate = w2.recordDate + interval 1 day
+    and w1.temperature > w2.temperature
