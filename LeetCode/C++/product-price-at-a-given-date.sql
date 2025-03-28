8cd180685a887029bf5e01b1058b01f54b708c20
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-03-28 17:01:56 +0800

    other approach

--- LeetCode/C++/product-price-at-a-given-date.sql
+++ LeetCode/C++/product-price-at-a-given-date.sql
@@ -1,15 +1,16 @@
-select distinct p.product_id
-    , ifnull(t.new_price, 10) as price
-from Products p
-left join (
-    select product_id
-        , new_price
-    from Products
-    where (product_id, change_date) in (
+select product_id
+    , new_price as price
+from Products
+where (product_id, change_date) in (
         select product_id
             , max(change_date)
         from Products
-        where change_date <= '2019-8-16'
+        where change_date <= '2019-08-16'
         group by product_id
     )
-) t on p.product_id = t.product_id
+union
+select distinct product_id
+    , 10 as price
+from Products
+group by product_id
+having min(change_date) > '2019-08-16'
