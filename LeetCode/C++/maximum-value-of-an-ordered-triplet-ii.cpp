b6f3285b6f87751045883342d54a267a2b850cf8
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-04-03 10:04:16 +0800

    O(n) time, O(1) space

--- LeetCode/C++/maximum-value-of-an-ordered-triplet-ii.cpp
+++ LeetCode/C++/maximum-value-of-an-ordered-triplet-ii.cpp
@@ -3,17 +3,14 @@ typedef long long ll;
 class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
-        ll ret = 0;
         int n = nums.size();
-        vector<int> sufM(n, 0);
-        for (int i = n-1, m = 0; i >= 0; i--) {
-            m = max(m, nums[i]);
-            sufM[i] = m;
-        }
-    
-        for (int i = 1, m = 0; i < n-1; i++) {
-            m = max(m, nums[i-1]);
-            ret = max(ret, (ll)(m-nums[i])*sufM[i+1]);
+
+        int vi = nums[0], vd = nums[0]-nums[1];
+        ll ret = max(0ll, (ll)vd*nums[2]);
+        for (int k = 3; k < n; k++) {
+            vi = max(vi, nums[k-2]);
+            vd = max(vd, vi-nums[k-1]);
+            ret = max(ret, (ll)vd*nums[k]);
         }
 
         return ret;
