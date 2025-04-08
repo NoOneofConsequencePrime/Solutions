e52f2a34fad6c0ec030757b83e43b4e416028125
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-04-08 11:26:37 +0800

    O(n) time, O(n) space

--- LeetCode/C++/repeated-substring-pattern.cpp
+++ LeetCode/C++/repeated-substring-pattern.cpp
@@ -1,21 +1,7 @@
-repeated-substring-patternclass Solution {
-    private:
-        bool cmp(const string& a, const string& b) {
-            int i = 0;
-            for (int j = 0; j < b.length(); i++, j++) {
-                if (i >= a.length()) {i = 0;}
-                if (a[i] != b[j]) {return false;}
-            }
-            if (i != a.length()) {return false;}
-    
-            return true;
-        }
+class Solution {
     public:
         bool repeatedSubstringPattern(string s) {
-            for (int i = 1; i <= s.length(); i++) {
-                if (cmp(s.substr(0, i), s.substr(i))) {cout << i; return true;}
-            }
-            
-            return false;
+            string tmp = s+s;
+            return tmp.substr(1, tmp.length()-2).find(s) != string::npos;
         }
     };
