e72c226c5fad0e3945b66ca9f4b8d7ce8f85a513
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-04-08 10:37:26 +0800

    O(n) time, O(1) space

---
+++ LeetCode/C++/the-kth-factor-of-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int kthFactor(int n, int k) {
+            for (int i = 1, j = 0; i <= n; i++) {
+                if (n%i == 0) {
+                    j++;
+                    if (j == k) {return i;}
+                }
+            }
+    
+            return -1;
+        }
+    };
