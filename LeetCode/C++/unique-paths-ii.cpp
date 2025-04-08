5ed1f9ea0da3086fc3a6b34222023ae8cf046a32
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-04-08 15:42:55 +0800

    O(mn) time, O(mn) space

---
+++ LeetCode/C++/unique-paths-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+            int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+            vector<vector<int>> dp(m, vector<int>(n, 0));
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (obstacleGrid[i][j] == 1) {continue;}
+                    if (i == 0 && j == 0) {
+                        dp[i][j] = 1;
+                        continue;
+                    }
+                    if (i-1 >= 0) {dp[i][j] += dp[i-1][j];}
+                    if (j-1 >= 0) {dp[i][j] += dp[i][j-1];}
+                }
+            }
+    
+            return dp[m-1][n-1];
+        }
+    };
