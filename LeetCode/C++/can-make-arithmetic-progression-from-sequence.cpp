148d9df9838cf1507293db4660aa64bf1d2ab1ca
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-04-08 13:32:02 +0800

    O(n) time, O(n) space

--- LeetCode/C++/can-make-arithmetic-progression-from-sequence.cpp
+++ LeetCode/C++/can-make-arithmetic-progression-from-sequence.cpp
@@ -8,4 +8,26 @@ class Solution {
     
             return true;
         }
-    };
+    };aclass Solution {
+        public:
+            bool canMakeArithmeticProgression(vector<int>& arr) {
+                unordered_map<int, int> st;
+                int mi = INT_MAX, ma = INT_MIN;
+                for (int x : arr) {
+                    mi = min(mi, x);
+                    ma = max(ma, x);
+                    st[x]++;
+                }
+        
+                if (st.size() == 1) {return true;}
+        
+                int chg = (ma-mi)/(arr.size()-1);
+                for (int i = mi; i <= ma; i += chg) {
+                    if (st.count(i) == 0) {return false;}
+                    st[i]--;
+                    if (st[i] == 0) {st.erase(i);}
+                }
+        
+                return st.empty();
+            }
+        };
