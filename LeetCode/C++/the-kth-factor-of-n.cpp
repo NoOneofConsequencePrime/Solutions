3ccbf7f124499fdd469fc065a39867b895c27d2c
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-04-08 10:48:01 +0800

    O(sqrt(n)) time, O(sqrt(n)) space

--- LeetCode/C++/the-kth-factor-of-n.cpp
+++ LeetCode/C++/the-kth-factor-of-n.cpp
@@ -1,13 +1,19 @@
 class Solution {
     public:
         int kthFactor(int n, int k) {
-            for (int i = 1, j = 0; i <= n; i++) {
+            int rt = sqrt(n);
+            vector<int> a, b;
+            for (int i = 1; i <= rt; i++) {
                 if (n%i == 0) {
-                    j++;
-                    if (j == k) {return i;}
+                    a.push_back(i);
+                    b.push_back(n/i);
                 }
             }
+            reverse(b.begin(), b.end());
+            for (auto x : b) {
+                if (a.back() < x) {a.push_back(x);}
+            }
     
-            return -1;
+            return k <= a.size()? a[k-1] : -1;
         }
     };
