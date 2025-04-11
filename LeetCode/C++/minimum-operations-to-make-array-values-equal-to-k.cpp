1592abf7418de0c8c4f967d7975cd9be0837944d
NoOneofConsequencePrime <bryanzhao5@outlook.com>
2025-04-11 15:16:34 +0800

    O(nlogn) time, O(1) space

--- LeetCode/C++/minimum-operations-to-make-array-values-equal-to-k.cpp
+++ LeetCode/C++/minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,12 +1,17 @@
 class Solution {
     public:
         int minOperations(vector<int>& nums, int k) {
-            unordered_set<int> st;
-            for (int x : nums) {
-                if (x < k) {return -1;}
-                st.insert(x);
+            sort(nums.begin(), nums.end());
+    
+            int ret = 0;
+            if (nums[0] < k) {return -1;}
+            if (nums[0] > k) {ret++;}
+            for (int i = 1; i < nums.size(); i++) {
+                if (nums[i] == nums[i-1]) {continue;}
+                if (nums[i] < k) {return -1;}
+                if (nums[i] > k) {ret++;}
             }
     
-            return st.size() - st.count(k);
+            return ret;
         }
     };
